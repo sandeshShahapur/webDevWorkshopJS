a77e8be1bc2590a6fb0d4b9b9f7e49d621b42599
Sandesh Shahapur <110241292+sandeshShahapur@users.noreply.github.com>
2024-01-13 17:32:43 +0530

    Create index.js

---
+++ js/syntax/index.js
@@ -0,0 +1,55 @@
+// Declaring variables
+let age = 25;
+const name = "John";
+var score = 90; 
+
+// Reassigning variables
+age = 26;
+score = 95;
+
+// Creating arrays
+let numbers = [1, 2, 3, 4, 5];
+const fruits = ["apple", "banana", "orange"];
+
+// Accessing array elements
+console.log(numbers[0]); // Output: 1
+
+// Modifying array elements
+fruits.push("grape");
+numbers.pop();
+
+// Creating strings
+let greeting = "Hello, ";
+const subject = "world!";
+
+// Concatenating strings
+let message = greeting + subject;
+
+// String length
+console.log(message.length);
+
+// Normal function
+function add(a, b) {
+  return a + b;
+}
+
+// Arrow function
+const multiply = (a, b) => a * b;
+
+// Function invocation
+let sum = add(3, 4);
+let product = multiply(5, 6);
+
+// Creating objects
+let person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 30,
+};
+
+// Accessing object properties
+console.log(person.firstName); // Output: John
+
+// Modifying object properties
+person.age = 31;
+
